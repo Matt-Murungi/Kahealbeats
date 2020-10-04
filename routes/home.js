ee87b50ee733be10dd6c0e3a348065fab9668bbe
i-AM-Wanda <57886510+i-AM-Wanda@users.noreply.github.com>
2020-10-04 18:56:29 +0300

    add homepage

--- routes/home.js
+++ routes/home.js
@@ -4,23 +4,33 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   musicVideos = [
     {
-      link: "https://youtu.be/DeMc26Nkvp0", 
+      link: "https://www.youtube.com/embed/kUlMSoTs5PI?list=PLjSangC7SdqSXiEhP-GcoQHwRmK6qjqC_", 
+   
+      name: "Making Moves",
+      author: "Roy Nkangi & GRG"
+    },
+    {
+      link: "https://www.youtube.com/embed/BGY1BIBxS20", 
       name: "Gazing",
       author: "Roy Nkangi & GRG"
     },
     {
-      link: "https://youtu.be/DeMc26Nkvp0", 
-      name: "Gazing",
+      link: "https://www.youtube.com/embed/wlynl-vgPo0" , 
+      name: "Me to Me",
       author: "Roy Nkangi & GRG"
     },
     {
-      link: "https://youtu.be/DeMc26Nkvp0", 
-      name: "Gazing",
+      link: "https://www.youtube.com/embed/P3N7OttRn08", 
+      name: "Get Loose",
       author: "Roy Nkangi & GRG"
     },
     {
-      link: "https://youtu.be/DeMc26Nkvp0", 
-      name: "Gazing",
+      link: "https://www.youtube.com/embed/OgQePlVfer4", 
+      name: "No Time",
+      author: "Roy Nkangi & GRG"
+    },   {
+      link: "https://www.youtube.com/embed/TXhQz_fBuCs", 
+      name: "Never Knew",
       author: "Roy Nkangi & GRG"
     },
   ]
